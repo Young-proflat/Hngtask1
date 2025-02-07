dbad378f555522d9c0036fbd977bdb4ab6e5bfdf
Young-proflat <olatunbosunlateef6@gmail.com>
2025-02-07 13:32:17 +0000

    push

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def get_fun_fact(number):
     except requests.RequestException:
         return "No fact available"
 
-@app.route("api/classify-number", methods =["GET"])
+@app.route("/api/classify-number", methods =["GET"])
 def classify_number():
     number_param = request.args.get("number")
 
